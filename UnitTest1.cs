7c9dbfd3d4e1d7dccdab39d416f843c1d505bf73
morkva-b <118727151+morkva-b@users.noreply.github.com>
2023-10-11 14:04:40 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using AnalaizerClassLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("System.Data.SqlClient", @"Data Source=(local)\SQLEXPRESS;Initial Catalog=t1;Integrated Security=True", "CreateStackTests", DataAccessMethod.Sequential)]
+        public void TestCreateStack()
+        {
+            AnalaizerClass.expression = (string)TestContext.DataRow["TestValue"];
+            ArrayList result = AnalaizerClassLibrary.AnalaizerClass.CreateStack();
+
+            string expected = TestContext.DataRow["ResultValue"].ToString();
+            ArrayList expectedList = new ArrayList(expected.Split(' '));
+
+            // Порівнюємо отриманий результат з очікуваним
+            CollectionAssert.AreEqual(expectedList, result);
+        }
+    }
+}
